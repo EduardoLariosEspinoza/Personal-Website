cdd557fe286251ef1a13ce9f7434651e428508e3
Eduardo Larios Espinoza <elariosespinoza2@gmail.com>
2024-11-03 21:07:20 -0600

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  background: lightgreen;
+  position: fixed;
+`
+
+function App() {
+  
+
+  return (
+    <Nav>jnhgfhg</Nav>
+  )
+}
+
+export default App
