dd51d9e9acc8d6e3aa7a27b9976d6242a717c957
Eduardo Larios Espinoza <elariosespinoza2@gmail.com>
2024-12-28 14:49:48 -0600

    Skills section change

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,16 +1,16 @@
 import javascriptLogo from "../assets/images/skills/javascript-logo.png";
 import typescriptLogo from "../assets/images/skills/typescript-logo.png";
 import reactLogo from "../assets/react.svg";
-import htmlLogo from "../assets/images/skills/html-logo.png";
-import cssLogo from "../assets/images/skills/css-logo.png";
 import jestLogo from "../assets/images/skills/jest-logo.png";
+import htmlCssLogo from "../assets/images/skills/HTML-CSS-logo.png";
+import nodeLogo from "../assets/images/skills/nodejs-logo.png";
 
 export const techSills = [
   { skill: "JavaScript", img: javascriptLogo },
   { skill: "TypeScript", img: typescriptLogo },
   { skill: "React.js", img: reactLogo },
-  { skill: "HTML", img: htmlLogo },
-  { skill: "CSS", img: cssLogo },
+  { skill: "HTML + CSS", img: htmlCssLogo },
+  { skill: "Node.js", img: nodeLogo },
   { skill: "Jest", img: jestLogo },
 ];
 
